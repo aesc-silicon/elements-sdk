9f7f396c15da4dfa5e9bca1ea7dad98501dfa751
Daniel Schultz <dnltz@aesc-silicon.de>
2022-06-10 13:55:08 +0200

    hardware: Carbon1: DH-012: Update baremetal0
    Update linker-script and start to support interrupts.
    
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

--- hardware/scala/soc/Carbon1/DH012/baremetal0/carbon1.c
+++ hardware/scala/soc/Carbon1/DH012/baremetal0/carbon1.c
@@ -1,19 +1,74 @@
 #include "soc.h"
 #include "uart.h"
+#include "gpio.h"
+#include "mtimer.h"
+#include "i2c.h"
+#include "plic.h"
+
+#define PLIC_BASE	0xF00F0000
 
 extern void hang(void);
-extern void setup_sys(void (*)(void));
+extern void timer_enable(void);
+extern void timer_disable(void);
+extern void interrupt_enable(void);
+extern void interrupt_disable(void);
 
+static struct uart_driver uartStd;
+/*
+void isr_handle(unsigned int mcause)
+{
+	unsigned char chr;
+
+	interrupt_disable();
+	if (uart_irq_rx_ready(&uartStd)) {
+		uart_irq_rx_disable(&uartStd);
+
+		while (uart_getc(&uartStd, &chr) == 0) {
+			uart_putc(&uartStd, chr);
+		}
+
+		uart_irq_rx_enable(&uartStd);
+	}
+
+	interrupt_enable();
+}
+*/
 void _kernel(void)
 {
 	unsigned char prompt[4] = ">_ \0";
 
-	struct uart_driver uartStd;
+	struct plic_driver plic;
 
+	plic_init(&plic, PLIC_BASE);
 	uart_init(&uartStd, UARTSTDCTRL_BASE,
 		UART_CALC_FREQUENCY(UARTSTDCTRL_FREQ, UARTSTDCTRL_BAUD, 8));
 
+	interrupt_enable();
+	plic_irq_enable(&plic, 2);
+	uart_irq_rx_enable(&uartStd);
+
 	uart_puts(&uartStd, prompt);
+
+
+
+
+/*
+	unsigned char response[2];
+
+	struct gpio_driver gpioStatus;
+	struct mtimer_driver mtimer;
+	struct i2c_driver i2cA;
+*/
+
+
+//	mtimer_init(&mtimer, MTIMERCTRL_BASE);
+//	gpio_init(&gpioStatus, GPIOSTATUSCTRL_BASE);
+//	i2c_init(&i2cA, I2CCONTROLLERACTRL_BASE,
+//		I2C_CALC_CLOCK(I2CCONTROLLERACTRL_FREQ, I2C_SPEED_STANDARD));
+
+
+//	i2c_read(&i2cA, 0x49, 0x0, 2, response);
+
 
 	hang();
 }
--- hardware/scala/soc/Carbon1/DH012/baremetal0/start.s
+++ hardware/scala/soc/Carbon1/DH012/baremetal0/start.s
@@ -1,19 +1,25 @@
+.equ REGBYTES, 0x4
+
 .section .init
 .global hang
-.global setup_sys
+.global timer_enable
+.global timer_disable
+.global interrupt_enable
+.global interrupt_disable
 _head:
 	jal	_init_regs
+	jal	_init_bss
 	li	sp, 0x80000200
+	jal	_init_trap
+
 	j	_kernel
+	nop
+
 
 hang:
 	nop
 	beq	zero, zero, hang
 
-setup_sys:
-	csrw	mtvec, a0
-	uret
-	ret
 
 _init_regs:
 	li	x2 , 0xA2A2A2A2
@@ -46,4 +52,99 @@ _init_regs:
 	li	x29, 0xC9C9C9C9
 	li	x30, 0xD0D0D0D0
 	li	x31, 0xD1D1D1D1
+	ret
+
+
+_init_bss:
+	la	t0, __bss_start
+	la	t1, __bss_end
+loop_head:
+	sw	zero, 0(t0)
+	beq	t0, t1, loop_end
+	addi	t0, t0, 4
+	j	loop_head
+loop_end:
+	ret
+	nop
+
+
+_init_trap:
+	la	t0, _irq_wrapper
+	csrw	mtvec, t0
+	ret
+
+
+_irq_wrapper:
+	add	sp, sp, -12*REGBYTES
+	sw	a0, 1*REGBYTES(sp)
+	sw	ra, 2*REGBYTES(sp)
+	sw	t0, 3*REGBYTES(sp)
+
+	csrr	a0, mcause
+	jal	isr_handle
+
+	lw	a0, 1*REGBYTES(sp)
+	lw	ra, 2*REGBYTES(sp)
+	lw	t0, 3*REGBYTES(sp)
+	addi	sp, sp, 12*REGBYTES
+	mret
+
+isr_handle:
+	# disable rx irq
+	li	t0, 0xf0000000
+	sw	zero, 20(t0)
+
+loop_s:
+	lw	a0, 0(t0)
+	mv	t0, a0
+	srli	a0, a0, 16
+	beq	a0, zero, loop_e
+	mv	a0, t0
+	li	t0, 0xf0000000
+	sw	a0, 0(t0)
+	beq	zero, zero, loop_s
+
+loop_e:
+	# enable rx irq
+	li	t0, 0xf0000000
+	li	a0, 0x2
+	sw	a0, 16(t0)
+	sw	a0, 20(t0)
+	# claim irq
+	li	t0, 0xf00ff000
+	li	a0, 2
+	sw	a0, 4(t0)
+	ret
+
+
+timer_enable:
+	csrr	a0, mie
+	ori	a0, a0, 0x80
+	csrw	mie, a0
+	ret
+
+timer_disable:
+	csrr	a0, mie
+	xori	a0, a0, 0x80
+	csrw	mie, a0
+	ret
+
+interrupt_enable:
+	csrr	a0, mie
+	li	a1, 0x800
+	or	a0, a0, a1
+	csrw	mie, a0
+	csrr	a0, mstatus
+	ori	a0, a0, 0x8
+	csrw	mstatus, a0
+	ret
+
+interrupt_disable:
+	csrr	a0, mie
+	li	a1, 0x800
+	xor	a0, a0, a1
+	csrw	mie, a0
+	csrr	a0, mstatus
+	xori	a0, a0, 0x8
+	csrw	mstatus, a0
 	ret
